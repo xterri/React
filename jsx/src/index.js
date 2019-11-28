5e40636e50a79106b810999f0fba8187b3b84174
xterri <thuynh1989@gmail.com>
2019-11-27 20:13:43 -0700

    notes on jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,10 +4,27 @@ import ReactDOM from 'react-dom';
 
 // create react component - can be a function or class
 
+function getButtonText() {
+    return 'Click on me!';
+}
+
 // function component
+// "" (double quotes) = string in JSX
+// '' (single quotes) = non-JSX properties 
+// class = className in JSX (to avoid class functions vs class label in jsx)
 const App = function () {
+    // const buttonText = { text: 'Click here' };
+        // can be string, numbers, array, but not objects unless called properly (ex. buttonText.text)
+
     return (
-        <div>Hi, I'm a function!</div> 
+        <div>
+            <div>Hi, I'm a function!</div> 
+            <div>
+                <label className="label" htmlFor="name">Enter Name: </label>
+                <input id="name" type="text" />
+                <button style={{ border: '2px solid blue', backgroundColor: "black", color: "white"}}>{getButtonText()}</button>
+            </div>
+        </div>
     );
 }
 
