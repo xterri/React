3213487660e08ad8ea3718f7270d9a791e5a893b
xterri <thuynh1989@gmail.com>
2019-11-28 22:50:06 -0700

    passing props

---
+++ components/src/CommentDetail.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// get parent properties with 'props'; deconstruct with {} w/ prop objNames
+const CommentDetail = ({ avatarImg, name, date, comment} ) => {
+    return (
+        <div className="comment">
+            <a href="/" className="avatar">
+                <img alt="avatar" src={avatarImg} />
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    {name}
+                </a>
+                <div className="metadata">
+                    <span className="date">{date}</span>
+                </div>
+                <div className="text">{comment}</div>
+            </div>
+        </div>
+    );
+};
+
+export default CommentDetail;
--- components/src/index.js
+++ components/src/index.js
@@ -2,23 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 
+import CommentDetail from './CommentDetail';
+
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Author's Name
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Comment/BlogPost!</div>
-                </div>
-            </div>
+            <CommentDetail 
+                avatarImg={faker.image.avatar()}
+                name='Terri'
+                date='November 28'
+                comment='Comment here!'
+            />
+            <CommentDetail 
+                avatarImg={faker.image.avatar()}
+                name='Sam'
+                date='November 29'
+                comment='whaaat?!'
+            />
+            <CommentDetail 
+                avatarImg={faker.image.avatar()}
+                name='Rob'
+                date='November 30'
+                comment='comment details!'
+            />
         </div>
     );
 };
